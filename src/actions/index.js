ef83451eab7ec4a0d48649f3b1c9742c544d26c7
Ivan <ivan8830@yandex.ru>
2023-10-24 11:31:58 +0500

    createAsyncThunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,19 @@
-import { heroesFetching, heroesFetched, heroesFetchingError} from '../components/heroesList/heroesSlice';
-import { filtersFetching, filtersFetched, filtersFetchingError } from '../components/heroesFilters/filtersSlice';
+// import { heroesFetching, heroesFetched, heroesFetchingError} from '../components/heroesList/heroesSlice';
+// import { filtersFetching, filtersFetched, filtersFetchingError } from '../components/heroesFilters/filtersSlice';
 
-export const fetchHeroes = (request) => (dispatch) => {
-    dispatch(heroesFetching());
-    request("http://localhost:3001/heroes")
-        .then(data => dispatch(heroesFetched(data)))
-        .catch(() => dispatch(heroesFetchingError()))
-}
+// export const fetchHeroes = (request) => (dispatch) => {
+//     dispatch(heroesFetching());
+//     request("http://localhost:3001/heroes")
+//         .then(data => dispatch(heroesFetched(data)))
+//         .catch(() => dispatch(heroesFetchingError()))
+// }
 
-export const fetchFilters = (request) => (dispatch) => {
+/* export const fetchFilters = (request) => (dispatch) => {
     dispatch(filtersFetching());
     request("http://localhost:3001/filters")
         .then(data => dispatch(filtersFetched(data)))
         .catch(() => dispatch(filtersFetchingError()))
-}
+} */
 
 // export const heroesFetching = () => {
 //     return {
